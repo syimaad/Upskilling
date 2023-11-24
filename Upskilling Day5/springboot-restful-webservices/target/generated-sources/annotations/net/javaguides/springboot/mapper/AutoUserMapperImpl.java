86df224b0a05d52fe057df1b25fc175f8471ddc3
syimaad <91290750+syimaad@users.noreply.github.com>
2023-11-24 10:47:04 +0530

    Add files via upload

---
+++ Upskilling Day5/springboot-restful-webservices/target/generated-sources/annotations/net/javaguides/springboot/mapper/AutoUserMapperImpl.java
@@ -0,0 +1,45 @@
+package net.javaguides.springboot.mapper;
+
+import javax.annotation.processing.Generated;
+import net.javaguides.springboot.dto.UserDto;
+import net.javaguides.springboot.entity.User;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-03-17T12:47:43+0530",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
+)
+public class AutoUserMapperImpl implements AutoUserMapper {
+
+    @Override
+    public UserDto mapToUserDto(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserDto userDto = new UserDto();
+
+        userDto.setId( user.getId() );
+        userDto.setFirstName( user.getFirstName() );
+        userDto.setLastName( user.getLastName() );
+        userDto.setEmail( user.getEmail() );
+
+        return userDto;
+    }
+
+    @Override
+    public User mapToUser(UserDto userDto) {
+        if ( userDto == null ) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setId( userDto.getId() );
+        user.setFirstName( userDto.getFirstName() );
+        user.setLastName( userDto.getLastName() );
+        user.setEmail( userDto.getEmail() );
+
+        return user;
+    }
+}
